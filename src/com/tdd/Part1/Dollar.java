e96def4432f8c9c05579b321f6e5eea8dd5fca8e
Omar <omarkshaikh@gmail.com>
2023-08-05 13:09:24 +0400

    Added package for TDD and implemented Chapter 1,2 and 3

---
+++ src/com/tdd/Part1/Dollar.java
@@ -0,0 +1,17 @@
+package com.tdd.Part1;
+
+class Dollar {
+    int amount;
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier){
+        return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object object){
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
+}
