3d39a893a92923136e7b36c5ce34f151b28cdeff
Omar <omarkshaikh@gmail.com>
2023-08-05 14:22:13 +0400

    Added package for stack implementation using TDD

---
+++ src/com/tdd/Stack/Stack.java
@@ -0,0 +1,29 @@
+package com.tdd.Stack;
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
+public class Stack {
+    private final ArrayList<Object> stack;
+
+    public Stack() {
+        stack = new ArrayList<>();
+    }
+
+    public Object pop() {
+        if(stack.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        var lastElement = stack.get(stack.size() - 1);
+        stack.remove(lastElement);
+
+        return lastElement;
+    }
+
+    public void push(Object element) {
+        stack.add(element);
+    }
+
+    public Object peek(){
+        return stack.get(stack.size() - 1);
+    }
+}
---
+++ src/com/tdd/Stack/StackTest.java
@@ -0,0 +1,48 @@
+package com.tdd.Stack;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
+
+class StackTest {
+    private final Object object1 = new Object();
+    private final Object object2 = new Object();
+
+    private Stack stack;
+
+    @BeforeEach
+    public void init() {
+        stack = new Stack();
+    }
+
+    @Test
+    public void testPushItemToStack(){
+        stack.push(object1);
+        assertEquals(object1, stack.peek());
+
+    }
+
+    @Test
+    public void testPopItemWhenStackEmpty() {
+        Stack emptyStack = new Stack();
+        assertThrows(EmptyStackException.class, emptyStack::pop);
+    }
+
+    @Test
+    public void testPopLastItemPushed() {
+        stack.push(object1);
+        assertEquals(object1, stack.pop());
+    }
+
+    @Test
+    public void testPopLastTwoItemsPushed()
+    {
+        stack.push(object1);
+        stack.push(object2);
+
+        assertEquals(object2, stack.pop());
+        assertEquals(object1, stack.pop());
+    }
+}
