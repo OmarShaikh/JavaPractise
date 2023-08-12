c8179c88d33ebf841a1ae7c7c37bda9b9dfb40ce
Omar <omarkshaikh@gmail.com>
2023-08-12 19:41:01 +0400

    Added comments for tests

--- src/com/tdd/Part1/DollarTest.java
+++ src/com/tdd/Part1/DollarTest.java
@@ -4,10 +4,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DollarTest {
+
+    // SETUP
+    // EXERCISE
+    // VERIFY
+    // TEARDOWN
     @Test
     public void testMulitplication() {
+
+        // SETUP
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
+
+        // VERIFY
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15,product.amount);
@@ -15,6 +24,8 @@ class DollarTest {
 
     @Test
     public void testEquality() {
+
+        // VERIFY
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
--- src/com/tdd/Stack/StackTest.java
+++ src/com/tdd/Stack/StackTest.java
@@ -7,9 +7,14 @@ import org.junit.jupiter.api.Test;
 import java.util.EmptyStackException;
 
 class StackTest {
+
+    // SETUP
+    // EXERCISE
+    // VERIFY
+    // TEARDOWN
+
     private final Object object1 = new Object();
     private final Object object2 = new Object();
-
     private Stack stack;
 
     @BeforeEach
@@ -19,13 +24,16 @@ class StackTest {
 
     @Test
     public void testPushItemToStack(){
+        // SETUP
         stack.push(object1);
+
+        // VERIFY
         assertEquals(object1, stack.peek());
     }
 
     @Test
     public void testStackFullWhilePushingNewItemToStack(){
-
+        // SETUP
         RuntimeException thrown = assertThrows(RuntimeException.class, () -> {
             stack.push(object1);
             stack.push(object1);
@@ -39,27 +47,36 @@ class StackTest {
             stack.push(object1);
             stack.push(object1);
         });
+
+        // VERIFY
         assertEquals("Stack is full", thrown.getMessage());
     }
 
     @Test
     public void testPopItemWhenStackEmpty() {
+        // SETUP
         Stack emptyStack = new Stack();
+
+        // VERIFY
         assertThrows(EmptyStackException.class, emptyStack::pop);
     }
 
     @Test
     public void testPopLastItemPushed() {
+        // SETUP
         stack.push(object1);
+
+        // VERIFY
         assertEquals(object1, stack.pop());
     }
 
     @Test
-    public void testPopLastTwoItemsPushed()
-    {
+    public void testPopLastTwoItemsPushed() {
+        // SETUP
         stack.push(object1);
         stack.push(object2);
 
+        // VERIFY
         assertEquals(object2, stack.pop());
         assertEquals(object1, stack.pop());
     }
